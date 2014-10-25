e95a990ab0c7c13e86f1932b7fe0ca1df902e793
yjxiong <xy012@ie.cuhk.edu.hk>
2014-10-26 00:31:01 +0800

    integrated multilabel

--- examples/ImageEvent/mpi_finetune_image_event.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-GOOGLE_LOG_DIR=models/ImageEvent/log \
-mpirun -np 4 \
-./build/tools/caffe train \
-    --solver=models/ImageEvent/solver.prototxt \
-    --weights=models/ImageEvent/googlenet_train_iter_270000.caffemodel
-    #--snapshot=models/ImageEvent/snapshots/ImageEvent_finetune_Overfeat_iter_3000.solverstate
-#    --gpu=3
--- examples/ImageEvent/sample_mpi.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-GOOGLE_LOG_DIR=models/googlenet/log \
-mpirun -np 2 ./build/tools/caffe train \
-    --solver=models/googlenet/solver.prototxt \
-    --snapshot=models/googlenet/googlenet_models/googlenet_train_iter_115000.solverstate
-#    --gpu=3
