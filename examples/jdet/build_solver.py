5a099f4c84d16409207dcb3e0fc8888539ed54d7
yjxiong <xy012@ie.cuhk.edu.hk>
2015-05-06 22:13:00 +0800

    change scripts

--- examples/jdet/build_solver.py
+++ examples/jdet/build_solver.py
@@ -10,6 +10,11 @@ from google.protobuf import text_format
 net_name = sys.argv[1]
 device_id = sys.argv[2]
 
+if len(sys.argv) == 4:
+  testing = True if sys.argv[3] == 'y' else False
+else:
+  testing = False
+
 solver_temp = 'models/jdet/solver.prototxt.example'
 base_path = '/media/datadisk_c/snapshots/jdet/'
 
@@ -17,14 +22,14 @@ starting_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 solver_name = 'models/jdet/solver_{}.prototxt'.format(net_name)
 proto_name = 'models/jdet/{}_train_val.prototxt'.format(net_name)
-snapshot_name = '{}_{}/{}'.format(net_name, date.today().isoformat(), starting_time)
+snapshot_name = '{}_{}/{}_gpu_{}'.format(net_name, date.today().isoformat(), starting_time, device_id)
 snapshot_dir = base_path + '{}'.format(snapshot_name)
 log_dir = os.path.join(snapshot_dir, 'log')
 
-if not os.path.isdir(snapshot_dir):
+if not os.path.isdir(snapshot_dir) and not testing:
   os.makedirs(snapshot_dir)
 
-if not os.path.isdir(log_dir):
+if not os.path.isdir(log_dir) and not testing:
   os.makedirs(log_dir)
 
 snapshot_path = os.path.join(snapshot_dir, net_name)
@@ -38,9 +43,15 @@ solver.net = proto_name
 solver.device_id = int(device_id)
 solver.snapshot_prefix = snapshot_path
 
+if testing:
+  solver.snapshot = 0
+
 #output the solver
-open(solver_name, 'w').write(text_format.MessageToString(solver))
-shutil.copy(proto_name, os.path.join(snapshot_dir, '{}_proto_{}.prototxt'.format(net_name, starting_time)))
-shutil.copy(solver_name, os.path.join(snapshot_dir, '{}_solver_{}.prototxt'.format(net_name, starting_time)))
+if not testing:
+  open(solver_name, 'w').write(text_format.MessageToString(solver))
+  shutil.copy(proto_name, os.path.join(snapshot_dir, '{}_proto_{}.prototxt'.format(net_name, starting_time)))
+  shutil.copy(solver_name, os.path.join(snapshot_dir, '{}_solver_{}.prototxt'.format(net_name, starting_time)))
+else:
+  log_dir="/media/datadisk_c/snapshots/tmp"
 
 print log_dir
--- examples/jdet/train_network.sh
+++ examples/jdet/train_network.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 NET=clarify
 DEVICE_ID=${1:-0}  
+TESTING=$2
 
-LOG_DIR=`~/anaconda/bin/ipython examples/jdet/build_solver.py $NET $DEVICE_ID`
+LOG_DIR=`~/anaconda/bin/python examples/jdet/build_solver.py $NET $DEVICE_ID $TESTING`
 
 echo Log path $LOG_DIR
 GOOGLE_LOG_DIR=$LOG_DIR ./build/tools/caffe train \
-    --solver=models/jdet/solver_$NET.prototxt
+    --solver=models/jdet/solver_$NET.prototxt  \
+    --weights=/media/ssd/caffe_bvlc/models/jdet/pretrained/clarify/origin_clarify.caffemodel
