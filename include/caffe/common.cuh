1b5d67da9adf45ce5bd3feeb30595c416d6292cf
lxx1991 <lxx1991@gmail.com>
2017-02-15 12:42:55 +0800

    clean code

--- include/caffe/common.cuh
+++ include/caffe/common.cuh
@@ -5,43 +5,6 @@
 
 #include <cuda.h>
 
-#include "caffe/half/half.hpp"
-#include "caffe/half/half_util.hpp"
-
-static __inline__ __device__ caffe::Half atomicAdd(caffe::Half* address, caffe::Half val) {
-#if __CUDA_ARCH__ >= 530 || !defined(__CUDA_ARCH__)
-  union U {
-      unsigned int i;
-      __half2 h;
-   };
-   union Up {
-      unsigned int * i;
-      __half2 * h;
-   };
-   Up up;
-   __shared__ __half2 temp_address;
-   temp_address.x = ((__half)(*address)).x;
-   up.h = &temp_address;
-   U old;
-   (old.h).x = ((__half)(*address)).x;
-   U assumed;
-   do {
-      assumed.i = old.i;
-      U temp;
-      __half2 temp_val;
-      temp_val.x = ((__half)val).x;
-      temp.h = __hadd2(temp_val, assumed.h);
-      old.i = atomicCAS( up.i, assumed.i, temp.i);
-   } while (assumed.i != old.i );
-   __half temp_val;
-   temp_val.x = (old.h).x;
-   return (caffe::Half)temp_val;
-#else
-   assert(false);
-   return caffe::Half(.0f);
-#endif
-}
-
 
 // CUDA: atomicAdd is not defined for doubles
 #if !defined(__CUDA_ARCH__) || __CUDA_ARCH__ >= 600
