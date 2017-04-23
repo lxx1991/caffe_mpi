6ff419692aa42abcdb6bb8529e62b427f113c830
lxx1991 <lxx1991@gmail.com>
2017-04-23 23:58:54 +0800

    fix bug

--- examples/semantic_segmentation/train_res_50_video_cityscapes.sh
+++ examples/semantic_segmentation/train_res_50_video_cityscapes.sh
@@ -5,3 +5,5 @@ GOOGLE_LOG_DIR=models/semantic_segmentation/fcn_res_50_cityscapes/log_aug_10 \
     build/install/bin/caffe train \
     --solver=models/semantic_segmentation/fcn_res_50_cityscapes/fcn_res_50_video_solver.prototxt \
     --weights=models/pretrained/initmodel/resnet50.caffemodel
+    #--snapshot=models/semantic_segmentation/fcn_res_50_cityscapes/snapshot_aug_10/fcn_res_50_iter_100000.solverstate
+    #--weights=models/pretrained/initmodel/resnet50.caffemodel
