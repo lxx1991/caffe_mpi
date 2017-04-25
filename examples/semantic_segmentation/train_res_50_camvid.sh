4ffa16ef95e232650ceb6c80ce025504882aa04a
lxx1991 <lxx1991@gmail.com>
2017-04-25 19:50:48 +0800

    camvid flownet

--- examples/semantic_segmentation/train_res_50_camvid.sh
+++ examples/semantic_segmentation/train_res_50_camvid.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-GOOGLE_LOG_DIR=models/semantic_segmentation/fcn_res_50_camvid/log \
-    /usr/local/openmpi/bin/mpirun -np 2 \
+#GOOGLE_LOG_DIR=models/semantic_segmentation/fcn_res_50_camvid/log \
+#    /usr/local/openmpi/bin/mpirun -np 1 \
     build/install/bin/caffe train \
     --solver=models/semantic_segmentation/fcn_res_50_camvid/fcn_res_50_solver.prototxt \
     --weights=models/pretrained/initmodel/resnet50.caffemodel
