499542bd480154e7d1166249d1b8b8301e2da4d0
lxx1991 <lxx1991@gmail.com>
2017-04-28 02:21:24 +0800

    bash

--- examples/semantic_segmentation/train_res_50_video_camvid.sh
+++ examples/semantic_segmentation/train_res_50_video_camvid.sh
@@ -3,5 +3,5 @@
 GOOGLE_LOG_DIR=models/semantic_segmentation/fcn_res_50_camvid/log \
     /usr/local/openmpi/bin/mpirun -np 8 \
     build/install/bin/caffe train \
-    --solver=models/semantic_segmentation/fcn_res_50_camvid/fcn_res_50_solver.prototxt \
+    --solver=models/semantic_segmentation/fcn_res_50_camvid/fcn_res_50_video_solver.prototxt \
     --weights=models/pretrained/initmodel/resnet50.caffemodel
